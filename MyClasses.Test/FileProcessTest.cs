6b046e89be3845fe487d1855a490c23dd3af3479
Roberto G. <roberto.manuel.balderas.gamez@hotmail.com>
2018-08-27 20:44:20 -0500

    Preparando proyecto para pruebas unitarias

---
+++ MyClasses.Test/FileProcessTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyClasses.Test
+{
+    [TestClass]
+    public class FileProcessTest
+    {
+        [TestMethod]
+        public void FileNameDoesExist()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void FileNameDoesNotExist()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void FileNameNullOrEmpty_ThrowsArgumentNullException()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
