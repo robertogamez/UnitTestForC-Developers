15d18e7783e391ea74093572fb6ed5f8de87e4bd
Roberto G. <roberto.manuel.balderas.gamez@hotmail.com>
2018-09-03 22:13:46 -0500

    Implementando pruebas unitarias implementado AAA

--- MyClasses.Test/FileProcessTest.cs
+++ MyClasses.Test/FileProcessTest.cs
@@ -9,19 +9,49 @@ namespace MyClasses.Test
         [TestMethod]
         public void FileNameDoesExist()
         {
-            Assert.Inconclusive();
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+
+            fromCall = fp.FileExists(@"C:\Windows\RegEdit.exe");
+
+            Assert.IsTrue(fromCall);
         }
 
         [TestMethod]
         public void FileNameDoesNotExist()
         {
-            Assert.Inconclusive();
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+
+            fromCall = fp.FileExists(@"C:\BadFolder\RegEdit.exe");
+
+            Assert.IsFalse(fromCall);
         }
 
         [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
         public void FileNameNullOrEmpty_ThrowsArgumentNullException()
         {
-            Assert.Inconclusive();
+            FileProcess fp = new FileProcess();
+
+            fp.FileExists("");
+        }
+
+        [TestMethod]
+        public void FileNameNullOrEmpty_ThrowsArgumentNullException_UsingTryCatch()
+        {
+            FileProcess fp = new FileProcess();
+
+            try
+            {
+                fp.FileExists("");
+            }
+            catch (ArgumentNullException)
+            {
+                return;
+            }
+
+            Assert.Fail("Call to FileExists method Not Throw ArgumentNullException");
         }
     }
 }
