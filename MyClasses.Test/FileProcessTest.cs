13e850e3d0571da0b254e732457b96003c25082c
Roberto Manuel Balderas Gamez <vn00m8d@mx.wal-mart.com>
2018-09-05 08:12:02 -0500

    Omitiendo datos duros para realizar las pruebas y preparando las clases de inicializacion y limpieza para pruebas

--- MyClasses.Test/FileProcessTest.cs
+++ MyClasses.Test/FileProcessTest.cs
@@ -1,18 +1,31 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Configuration;
+using System.IO;
 
 namespace MyClasses.Test
 {
     [TestClass]
     public class FileProcessTest
     {
+        private const string BAD_FILE_NAME = @"C:\BadFolder\RegEdit.exe";
+        private string _GoodFileName;
+
+        public TestContext TestContext { get; set; }
+
         [TestMethod]
         public void FileNameDoesExist()
         {
             FileProcess fp = new FileProcess();
             bool fromCall;
 
-            fromCall = fp.FileExists(@"C:\Windows\RegEdit.exe");
+            SetGoodFileName();
+            TestContext.WriteLine("Creating the file: " + _GoodFileName);
+            File.AppendAllText(_GoodFileName, "Some Text");
+            TestContext.WriteLine("Testing the file: " + _GoodFileName);
+            fromCall = fp.FileExists(_GoodFileName);
+            TestContext.WriteLine("Deleting the file: " + _GoodFileName);
+            File.Delete(_GoodFileName);
 
             Assert.IsTrue(fromCall);
         }
@@ -23,7 +36,7 @@ namespace MyClasses.Test
             FileProcess fp = new FileProcess();
             bool fromCall;
 
-            fromCall = fp.FileExists(@"C:\BadFolder\RegEdit.exe");
+            fromCall = fp.FileExists(BAD_FILE_NAME);
 
             Assert.IsFalse(fromCall);
         }
@@ -35,6 +48,16 @@ namespace MyClasses.Test
             FileProcess fp = new FileProcess();
 
             fp.FileExists("");
+        }
+
+        public void SetGoodFileName()
+        {
+            _GoodFileName = ConfigurationManager.AppSettings["GoodFileName"];
+            if (_GoodFileName.Contains("[AppPath]"))
+            {
+                _GoodFileName = _GoodFileName.Replace("[AppPath]",
+                    Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+            }
         }
 
         [TestMethod]
---
+++ MyClasses.Test/MyClassesTestInitialization.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyClasses.Test
+{
+    /// <summary>
+    /// Assembly Initialize and Cleanup Methods
+    /// </summary>
+    [TestClass]
+    public class MyClassesTestInitialization
+    {
+        [AssemblyInitialize]
+        public static void AssemblyInitialize(TestContext tc)
+        {
+            tc.WriteLine("In the Assembly Initialize Method.");
+            // TODO: Create resources needed for your tests.
+        }
+
+        [AssemblyCleanup]
+        public static void AssemblyCleanup()
+        {
+            // TODO: Clean up any resources used by your tests.
+        }
+    }
+}
